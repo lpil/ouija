1aa333439f59be422fa1912024a188af0bd82881
Louis Pilfold <louis@lpil.uk>
2015-10-04 20:41:56 +0100

    We are humans

--- lib/ouija/endpoint.ex
+++ lib/ouija/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Ouija.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :ouija, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico humans.txt robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
